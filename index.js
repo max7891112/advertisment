628ddc812d28cda527a9fe5b6c4c4189f4c698d4
max7891112 <max.alkhimovich@gmail.com>
2023-06-12 15:49:18 +1000

    v.2.1 smoothScroll

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import Why from "./components/Why/why";
 import OurTeam from "./components/Our-team/ourTeam";
 import Cases from "./components/Cases/cases";
 import Footer from './components/Footer/footer';
+import SmoothScroll from "smoothscroll-for-websites";
 document.addEventListener('DOMContentLoaded', () => {
     
     MenuBurger.clickOnBurger();
@@ -35,6 +36,28 @@ document.addEventListener('DOMContentLoaded', () => {
     Cases.addEventListener();
     Cases.addELClick();
     Footer.addEventListener();
+
+    SmoothScroll({ animationTime    : 600, // [ms]
+    stepSize         : 100, // [px]
+
+    // Acceleration
+    accelerationDelta : 50,  // 50
+    accelerationMax   : 3,   // 3
+
+    // Keyboard Settings
+    keyboardSupport   : true,  // option
+    arrowScroll       : 50,    // [px]
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+
+    // Other
+    touchpadSupport   : false, // ignore touchpad by default
+    fixedBackground   : true, 
+    excluded          : ''     })
 })
 
 
