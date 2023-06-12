ca39137c0b8cc0821e67041bee093d185912a5dc
max7891112 <max.alkhimovich@gmail.com>
2023-06-12 20:46:51 +1000

    v2.2

--- components/workWithUs/WorkWithUs.js
+++ components/workWithUs/WorkWithUs.js
@@ -26,7 +26,7 @@ class WorkWithUs {
                 </div>
                 
                 <div class="react-component__video-container">
-                    <a href=${path} class="react-component__link" data-youtubeLightbox="true">
+                    <a class="react-component__link" data-youtubeLightbox="true">
                         <img src=${image} alt="#" class="react-component__img">
                         <img src=${playIcon} alt="#" class="_icon">
                     </a>
@@ -60,7 +60,7 @@ class WorkWithUs {
         this.render(logo, image, text, path, href);
         document.querySelector('.react-component').classList.add('_open');
         this.addListenerForPrepareLightBox()
-        youtubeLightbox.connect('youtubelightbox', 'youtubelightboxPlayer', '.react-component__video-container')
+        // youtubeLightbox.connect('youtubelightbox', 'youtubelightboxPlayer', '.react-component__video-container')
     }
 
     addListenerForChangingComponents() {
@@ -70,28 +70,65 @@ class WorkWithUs {
         let prostor = document.getElementById('prostor');
         let malina = document.getElementById('malina');
         let dragon = document.getElementById('dragon');
+        let allButtons = document.querySelectorAll('.work-with-us__item')
         strata.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            strata.classList.add('_active');
+            strata.setAttribute('data-active', 'true')
             this.open(strataLogo, book, `Итальянский производитель и поставщик одежды, основанный в 1974 году. 
             Является поставщиком дизайнерской одежды во всём мире. Штаб-квартира компании располагается в Барселоне.`, 'https://www.youtube.com/watch?v=jyetqrX2hYA',
             "./components/workWithUs/loginova/loginova.html")
         });
         manue.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            manue.classList.add('_active');
+            manue.setAttribute('data-active', 'true')
             this.open(dreamGirl, oldPhone, `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!`, 'https://www.youtube.com/watch?v=Z7wUyXJSHnk',
             "./components/workWithUs/levina/levina.html")
         });
         serenity.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            serenity.classList.add('_active');
+            serenity.setAttribute('data-active', 'true')
             this.open(skate, dreamingGuy, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi officia harum corporis explicabo laborum
              voluptates est eligendi possimus? Tempore!`, 'https://www.youtube.com/watch?v=jyetqrX2hYA', "./components/workWithUs/larionov/larionov.html")
         });
         prostor.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            prostor.classList.add('_active');
+            prostor.setAttribute('data-active', 'true')
             this.open(crazyGuitar, playingGuitar, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!', 'https://www.youtube.com/watch?v=Z7wUyXJSHnk',
             "./components/workWithUs/belov/belov.html")
         });
         malina.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            malina.classList.add('_active');
+            malina.setAttribute('data-active', 'true')
             this.open(ultrafioletGirl, jumpGuy, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi officia harum corporis explicabo laborum voluptates 
             est eligendi possimus? Tempore!`, 'https://www.youtube.com/watch?v=jyetqrX2hYA', '#')
         });
         dragon.addEventListener('click', () => {
+            for(let button of allButtons) {
+                button.classList.remove('_active')
+                button.setAttribute('data-active', 'false')
+            }
+            dragon.classList.add('_active');
+            dragon.setAttribute('data-active', 'true')
             this.open(japan, eye, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!', 'https://www.youtube.com/watch?v=Z7wUyXJSHnk', "#")
         });
     };
@@ -99,18 +136,36 @@ class WorkWithUs {
     addListenerForPrepareLightBox() {
         let target = document.querySelector('.react-component__link')
         target.addEventListener('click', () => {
-            document.querySelector('.react-component__dinamic-block-container')
-            .classList.add('_increase');
-            document.querySelector('.work-with-us')
-            .classList.add('_increase');
+            let container = document.querySelector('.react-component__dinamic-block-container')
+            if(!container.getAttribute('data-open-elem')) {
+                container.classList.add('_increase');
+                container.setAttribute('data-open-elem', 'true')
+                document.querySelector('.work-with-us')
+                    .classList.add('_increase');
+            } else {
+                REACT_COMPONENT.innerHTML = "";
+                if(strata.getAttribute('data-active') == 'true') {
+                    this.open(strataLogo, book, `Итальянский производитель и поставщик одежды, основанный в 1974 году. 
+                        Является поставщиком дизайнерской одежды во всём мире. Штаб-квартира компании располагается в Барселоне.`, 'https://www.youtube.com/watch?v=jyetqrX2hYA',
+                        "./components/workWithUs/loginova/loginova.html")
+                } else if(manue.getAttribute('data-active') == 'true') {
+                        this.open(dreamGirl, oldPhone, `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!`, 'https://www.youtube.com/watch?v=Z7wUyXJSHnk',
+                        "./components/workWithUs/levina/levina.html")
+                } else if(serenity.getAttribute('data-active') == 'true') {
+                        this.open(skate, dreamingGuy, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi officia harum corporis explicabo laborum
+                        voluptates est eligendi possimus? Tempore!`, 'https://www.youtube.com/watch?v=jyetqrX2hYA', "./components/workWithUs/larionov/larionov.html")
+                } else if(prostor.getAttribute('data-active') == 'true') {
+                        this.open(crazyGuitar, playingGuitar, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!', 'https://www.youtube.com/watch?v=Z7wUyXJSHnk',
+                        "./components/workWithUs/belov/belov.html")
+                } else if(malina.getAttribute('data-active') == 'true') {
+                        this.open(ultrafioletGirl, jumpGuy, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sequi officia harum corporis explicabo laborum voluptates 
+                        est eligendi possimus? Tempore!`, 'https://www.youtube.com/watch?v=jyetqrX2hYA', '#')
+                } else {
+                    this.open(japan, eye, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, minima!', 'https://www.youtube.com/watch?v=Z7wUyXJSHnk', "#")
+                };
+            };
         });
-        target.addEventListener('dblclick', () => {
-            document.querySelector('.react-component__dinamic-block-container')
-            .classList.remove('_increase');
-            document.querySelector('.work-with-us')
-            .classList.remove('_increase');
-        });
-    }
+    };
 };
 
 export default new WorkWithUs();
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Why from "./components/Why/why";
 import OurTeam from "./components/Our-team/ourTeam";
 import Cases from "./components/Cases/cases";
 import Footer from './components/Footer/footer';
-import SmoothScroll from "smoothscroll-for-websites";
+import MySmoothScroll from './libs/SmoothScroll.js'
 document.addEventListener('DOMContentLoaded', () => {
     
     MenuBurger.clickOnBurger();
@@ -37,27 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Cases.addELClick();
     Footer.addEventListener();
 
-    SmoothScroll({ animationTime    : 600, // [ms]
-    stepSize         : 100, // [px]
-
-    // Acceleration
-    accelerationDelta : 50,  // 50
-    accelerationMax   : 3,   // 3
-
-    // Keyboard Settings
-    keyboardSupport   : true,  // option
-    arrowScroll       : 50,    // [px]
-
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm   : true,
-    pulseScale       : 4,
-    pulseNormalize   : 1,
-
-    // Other
-    touchpadSupport   : false, // ignore touchpad by default
-    fixedBackground   : true, 
-    excluded          : ''     })
+    MySmoothScroll.render()
 })
 
 
