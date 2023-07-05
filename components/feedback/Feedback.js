441ae5d7ffb4eec61af4ace342f36a7dddd04949
max7891112 <max.alkhimovich@gmail.com>
2023-07-05 17:29:32 +1000

    v2.1

--- components/feedback/Feedback.js
+++ components/feedback/Feedback.js
@@ -1,5 +1,5 @@
 import close from '../feedback//img/close.svg';
-import { ROOT_MODALS } from '../../constants/var';
+import { ROOT_MODAL } from '../../constants/var';
 class Feedback {
     render() {
         let htmlContent = ` 
@@ -33,40 +33,32 @@ class Feedback {
                 </div>
             `;
 
-            for(let ROOT_MODAL of ROOT_MODALS ) {
-                ROOT_MODAL.innerHTML = htmlWrapper;
-            }
+
+            ROOT_MODAL.innerHTML = htmlWrapper;
+            
     }
     
     open() {
         this.render();
-        let modalWindows = document.querySelectorAll('.modal__body')
-        for(let body of modalWindows) {
-            body.classList.add('open');
-        }
-        let modals = document.querySelectorAll('.modal')
-        for(let modal of modals) {
-            modal.classList.add('open');
-        }
+        document.querySelector('.modal__body').classList.add('open')
+        document.querySelector('.modal').classList.add('open');
     }
 
     close() {   
-        let modals = document.querySelectorAll('.modal')
+        let modal = document.querySelector('.modal')
     
-        let modalWindows = document.querySelectorAll('.modal__body')
-        for(let i = 0; i < modals.length; i++) {
-            if(modals[i]) {
-                modalWindows[i].classList.remove('open');
-                modalWindows[i].parentElement.classList.remove('open');
-                modalWindows[i].classList.add('disappearance');
-                modalWindows[i].parentElement.classList.add('disappearance');
-                setTimeout(() => {
-                    modalWindows[i].classList.remove('disappearance');
-                    modalWindows[i].parentElement.classList.remove('disappearance');
-                    ROOT_MODALS[i].innerHTML = '';
-                },300);
-            };
-        }
+        let modalWindow = document.querySelector('.modal__body')
+        if(modal) {
+            modalWindow.classList.remove('open');
+            modalWindow.parentElement.classList.remove('open');
+            modalWindow.classList.add('disappearance');
+            modalWindow.parentElement.classList.add('disappearance');
+            setTimeout(() => {
+                modalWindow.classList.remove('disappearance');
+                modalWindow.parentElement.classList.remove('disappearance');
+                ROOT_MODAL.innerHTML = '';
+            },300);
+        };
     }; 
 
 
@@ -77,7 +69,6 @@ class Feedback {
 
             modalSubmit.addEventListener('click', formSend);
             function formSend (event){  // async
-
                 event.preventDefault();
 
                 let error = formValidate(form)
@@ -112,9 +103,7 @@ class Feedback {
                                 setTimeout(() => {
                                     modalWindow.classList.remove('disappearance');
                                     modalWindow.parentElement.classList.remove('disappearance');
-                                    for(let ROOT_MODAL of ROOT_MODALS ) {
-                                        ROOT_MODAL.innerHTML = '';
-                                    }
+                                    ROOT_MODAL.innerHTML = '';
                                 },300);
                             }; // дублирование кода
 
